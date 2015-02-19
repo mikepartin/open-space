720c0ee1572e7ff4ed9d970124ca7ba4ff0c76af
mike partin <mike.partin@gmail.com>
2015-02-19 03:14:43 -0500

    A Step In The Right Direction

--- openspace/OpenSpace.java
+++ openspace/OpenSpace.java
@@ -4,7 +4,14 @@ public class OpenSpace {
 
 	public static void main(String[] args) {
 
-		System.out.println("testing...");
+		//System.out.println("testing...");
+		displayMessage();
+
+	}
+
+	private static void displayMessage() {
+
+		System.out.println("Welcome to the beginning of a revolutionary game that will be open to all types of tweeking and customizing.");
 
 	}
 
